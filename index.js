a9571d6c7c290a50a0911c7b9b6e99b72990b54b
el-sam <samurai.h@yahoo.com>
2023-09-28 08:54:39 +0800

    copy the code and paste into function 'addingEventListener()' so to get test pass

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Create node for input element in DOM
+// const btn = document.getElementById("button");
+// const btn = document.querySelector("input#button");
+// btn.addEventListener('click',addingEventListener);
+
+/* Below is the code created for passing the test, so the listener is 
+created inside function '' addingEventListener()*/
 function addingEventListener() {
+    // Complete impletement of code below
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
 }
+
+
+
+// addingEventListener();
